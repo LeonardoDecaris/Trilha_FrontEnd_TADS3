ad5710b9cf7545ca2a40c0a65648bce05545d59f
Leonardo Nunes Decaris <leonardodecaris8040@gmail.com>
2025-04-06 13:14:47 -0300

    Trilha 1: Ex2 Card

--- trilha1_ex2/src/App.tsx
+++ trilha1_ex2/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Button from './components/Button'
+import ProfileCard from './components/ProfileCard'
 
 function App() {
 
   return (
     <>
       <Button /> 
+      <ProfileCard />
+      <ProfileCard />
     </>
   )
 }
